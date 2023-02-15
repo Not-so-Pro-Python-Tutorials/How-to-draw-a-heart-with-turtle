e2a8b369ee3d96b37b7200352f809b325fc4457c
Not-So-Pro Python Tutorials on YouTube <123256054+Not-so-Pro-Python-Tutorials@users.noreply.github.com>
2023-02-14 19:54:53 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+import time
+
+pen = Turtle()
+
+pen.speed(100)
+pen.fillcolor("red")
+pen.begin_fill()
+pen.left(140)
+pen.forward(113)
+
+for i in range(200):
+    pen.right(1)
+    pen.forward(1)
+
+pen.left(120)
+
+for i in range(200):
+    pen.right(1)
+    pen.forward(1)
+
+pen.forward(112)
+pen.end_fill()
+time.sleep(5)
